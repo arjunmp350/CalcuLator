2400d31d230d25b542658d8acff59a6530fa96db
Sidhu <sidharth.v.mtr@gmail.com>
2014-08-14 20:20:52 +0530

    Add_operation

--- app/src/main/java/com/calculator/pixel/calculator/MyActivity.java
+++ app/src/main/java/com/calculator/pixel/calculator/MyActivity.java
@@ -1,17 +1,37 @@
 package com.calculator.pixel.calculator;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MyActivity extends Activity {
+
+    private EditText Src;
+    private float numbBr;
+    private String operation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        Src = (EditText) findViewById(R.id.txtnum_inp);
+        Src.setEnabled(false);
+
+        int idList[] = {R.id.btn_no_0,R.id.btn_no_1,R.id.btn_no_2,R.id.btn_no_3,R.id.btn_no_4,R.id.btn_no_5,R.id.btn_no_6,R.id.btn_no_7,R.id.btn_no_8,
+        R.id.btn_no_9,R.id.btn_sym_div,R.id.btn_sym_dot,R.id.btn_sym_eq,R.id.btn_sym_min,R.id.btn_sym_multi,R.id.btn_sym_plus,R.id.btn_clr};
+
+        for (int id:idList) {
+            View v = findViewById(id);
+            v.setOnClickListener(BtnClk);
+
+        }
     }
 
 
@@ -21,6 +41,79 @@ public class MyActivity extends Activity {
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
     }
+
+
+    public void mMath(String str){
+        numbBr =Float.parseFloat(Src.getText().toString());
+        operation = str;
+        Src.setText("0");
+    }
+
+    public  void getKeyboard(String str){
+        String SrcCurrent = Src.getText().toString();
+        if (SrcCurrent.equals("0"))
+            SrcCurrent = " ";
+        SrcCurrent += str;
+        Src.setText(SrcCurrent);
+    }
+
+    public void mResult(){
+        float numbAf = Float.parseFloat(Src.getText().toString());
+        float result = 0 ;
+
+        if(operation.equals("+")) {
+            result = numbAf + numbBr;
+        }
+        if (operation.equals("-")) {
+            result = numbBr - numbAf;
+        }
+        if (operation.equals("*")) {
+            result = numbAf * numbBr;
+        }
+        if (operation.equals("/")) {
+            result = numbBr / numbAf;
+        }
+        Src.setText(String.valueOf(result));
+
+        }
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_clr:
+                    Src.setText("0");
+                    numbBr = 0;
+                    operation = "";
+                    break;
+                case R.id.btn_sym_plus:
+                    mMath("+");
+                    break;
+                case R.id.btn_sym_min:
+                    mMath("-");
+                    break;
+                case R.id.btn_sym_multi:
+                    mMath("*");
+                    break;
+                case R.id.btn_sym_div:
+                    mMath("/");
+                    break;
+                case R.id.btn_sym_eq:
+                    mResult();
+                    break;
+                default:
+                    String numb = ((Button) v).getText().toString();
+                    getKeyboard(numb);
+                    break;
+
+
+            }
+        }
+
+
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
